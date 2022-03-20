a4bc2cbab0a7788378c5ebb23b9bff1043c9cbcd
Aviv <aviv.dror@gmail.com>
2022-03-20 13:48:28 +0100

    pokemon recourse solution

--- 07-api-calls/pokemon/exercises/evolution-chain.js
+++ 07-api-calls/pokemon/exercises/evolution-chain.js
@@ -9,6 +9,7 @@ import { ORIGIN } from '../config.js';
  *
  * @throws {Error} HTTP error! status: {number}.
  */
+
 export const evolutionChain = async (chainId = 1) => {
   // --- generate and declare your resource's URL ---
   // docs: https://pokeapi.co/docs/v2#evolution-section
@@ -28,21 +29,41 @@ export const evolutionChain = async (chainId = 1) => {
 
   /* --- parse the data if the response was ok (this works!) ---*/
   const data = await response.json();
-  // console.log(data);
+  console.log(data);
   // --- process the fetched data (if necessary) ---
   //  you do not need to use `await` below this comment
   //  you can refactor this to a separate logic function and test it
   // tricky one!  you will need to push all the species into an array
+
   const pokemon = [];
   pokemon.push(data.chain.species);
-  const keys = Object.keys(data);
-  console.log(keys)
-  for (const key of keys) {
-    if (key === 'chain') {
-    }
+  let key = data.chain.evolves_to;
+  while (key.length === 1) {
+    pokemon.push(key[0].species);
+    key = key[0].evolves_to;
   }
   return pokemon;
 };
+
+/*
+  const pokemon = [];
+pokemon.push(data.chain.species);
+// console.log(pokemon)
+
+const keyOne = Object.keys(data.chain.evolves_to);
+// console.log(keys)
+
+if (keyOne.includes('0')) {
+  pokemon.push(data.chain.evolves_to[0].species);
+  // console.log(pokemon)
+}
+const keyTwo = Object.keys(data.chain.evolves_to[0].evolves_to)
+// console.log(keyTwo)
+if (keyTwo.includes('0')) {
+  pokemon.push(data.chain.evolves_to[0].evolves_to[0].species);
+  // console.log(pokemon)
+}
+
 // data -> chain -> evolves_to -> 0 -> evolves_to
 //            species             species
 /*
